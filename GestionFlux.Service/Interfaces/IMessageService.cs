e476a147213914de86a1c9b93adab91f333bc622
Harry Michael <ando.lalaina1103@gmail.com>
2022-03-15 11:47:03 +0300

    Add Message service

---
+++ GestionFlux.Service/Interfaces/IMessageService.cs
@@ -0,0 +1,22 @@
+﻿using GestionFlux.Domain.Models;
+using GestionFlux.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionFlux.Service.Interfaces
+{
+    public interface IMessageService
+    {
+        IEnumerable<Request> GetRequests();
+        IEnumerable<Request> GetRequests(User sender);
+        Request GetRequest(int id);
+        void InsertRequest(Request request);
+
+        IEnumerable<Notification> GetNotifications(User sender);
+        Notification GetNotification(int id);
+        void InsertNotification(Notification notification);
+    }
+}
---
+++ GestionFlux.Service/Services/MessageService.cs
@@ -0,0 +1,54 @@
+﻿using GestionFlux.Domain.Models;
+using GestionFlux.Repository;
+using GestionFlux.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionFlux.Service.Services
+{
+    public class MessageService : IMessageService
+    {
+        private Repository<Request> requestRepository;
+        private Repository<Notification> notificationRepository;
+
+        public MessageService(Repository<Request> requestRepo, Repository<Notification> notificationRepo)
+        {
+            this.requestRepository = requestRepo;
+            this.notificationRepository = notificationRepo;
+        }
+        public IEnumerable<Request> GetRequests()
+        {
+            return requestRepository.GetAll();
+        }
+        public IEnumerable<Request> GetRequests(User sender)
+        {
+            return requestRepository.GetAll().Where(x => x.Sender == sender);
+        }
+        public Request GetRequest(int id)
+        {
+            return requestRepository.Get(id);
+        }
+        public void InsertRequest(Request request)
+        {
+            requestRepository.Insert(request);
+        }
+
+        public IEnumerable<Notification> GetNotifications(User sendTo)
+        {
+            return notificationRepository.GetAll().Where(x => x.SendTo == sendTo);
+        }
+
+        public Notification GetNotification(int id)
+        {
+            return notificationRepository.Get(id);
+        }
+
+        public void InsertNotification(Notification notification)
+        {
+            notificationRepository.Insert(notification);
+        }
+    }
+}
